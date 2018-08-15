2b7bad9a1d9bad0294f767f477f017e4aee9fe1e
GerCassa <gercassa@gmail.com>
2018-08-15 11:52:15 +0100

    customer class method added

--- console.rb
+++ console.rb
@@ -2,28 +2,32 @@ require("pry-byebug")
 require_relative("models/pizza_order")
 require_relative("models/customer")
 
-# # PizzaOrder.delete_all()
-#
-#
+PizzaOrder.delete_all()
+Customer.delete_all()
+
+
   customer1 = Customer.new({
     "first_name" => "Darth",
     "last_name" => "Vader"
     })
 
-#
-# order1  = PizzaOrder.new({
-#   "first_name" => "Luke",
-#   "last_name" => "Skywalker",
-#   "quantity" => "1",
-#   "topping" => "Napoli"
-#   })
-#
-# order2 = PizzaOrder.new({
-#   "first_name" => "Darth",
-#   "last_name" => "Vader",
-#   "quantity" => "1",
-#   "topping" => "Qauttro Formaggi"
-#   })
+# customer1.save()
+
+
+order1  = PizzaOrder.new({
+  "quantity" => "1",
+  "topping" => "Napoli",
+  "customer_id" => customer1.id
+  })
+
+order2  = PizzaOrder.new({
+  "quantity" => "1",
+  "topping" => "Napoli",
+  "customer_id" => customer1.id
+  })
+
+
+
 #
 # #EVIL ORDER
 # # order3 = PizzaOrder.new({
@@ -34,7 +38,7 @@ require_relative("models/customer")
 # #   })
 #
 
-customer1.save()
+
 
 
 
--- models/pizza_order.rb
+++ models/pizza_order.rb
@@ -1,23 +1,25 @@
 require("pg")
+require_relative("customer")
 
 class PizzaOrder
-  attr_reader :id
-  attr_accessor :first_name, :last_name, :quantity, :topping
+  attr_reader :id, :customer_id
+  attr_accessor :quantity, :topping
 
   def initialize (options)
     @id = options["id"].to_i if options["id"]
     @quantity = options["quantity"].to_i
     @topping = options["topping"]
-    
+    @customer_id = options["customer_id"].to_i if options["customer_id"]
+
   end
 
   def save()
     db = PG.connect({ dbname: "pizza_shop", host: "localhost"})
     sql = "INSERT INTO pizza_orders
-        (quantity, topping)
+        (quantity, topping, customer_id)
         VALUES
-        ($1, $2, $3, $4) RETURNING *"
-    values = [@first_name, @last_name, @quantity, @topping]
+        ($1, $2, $3) RETURNING *"
+    values = [@quantity, @topping, @customer_id]
     db.prepare("save", sql)
     @id = db.exec_prepared("save", values)[0]["id"].to_i
     db.close()
@@ -37,15 +39,31 @@ class PizzaOrder
     db = PG.connect({ dbname: 'pizza_shop', host: 'localhost'})
     sql = "UPDATE pizza_orders
     SET
-    (first_name, last_name, quantity, topping)
+    (quantity, topping, customer_id)
     =
-    ($1,$2,$3,$4)
-    WHERE id = $5"
-    values = [@first_name, @last_name, @quantity, @topping, @id]
+    ($1,$2,$4)
+    WHERE id = $3"
+    values = [@quantity, @topping, @id, @customer_id]
     db.prepare("update", sql)
     db.exec_prepared("update", values)
     db.close()
   end
+
+  def customer
+    db = PG.connect({ dbname: 'pizza_shop', host: 'localhost'})
+    sql = "SELECT * FROM customers
+           WHERE customer_id = $1"
+    values = [@customer_id]
+    db.prepare("customer", sql)
+    result = db.exec_prepared("customer", values)
+    db.close()
+    return result[0]
+  end
+
+
+
+
+
 
 #using self also works to avoid writting class name.
   def PizzaOrder.all()
